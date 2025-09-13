85f9287d57de294289ebf6e9c4d4266bacf78a06
xserxses <filip.jan.misztal@gmail.com>
2025-09-13 21:19:31 +0200

    Implement generic error screen

--- composeApp/src/commonMain/kotlin/com/github/xserxses/orderbook/ui/composables/ErrorComposable.kt
+++ composeApp/src/commonMain/kotlin/com/github/xserxses/orderbook/ui/composables/ErrorComposable.kt
@@ -1,12 +1,27 @@
 package com.github.xserxses.orderbook.ui.composables
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.github.xserxses.orderbook.ui.OrderBookTheme
+import orderbook.composeapp.generated.resources.Res
+import orderbook.composeapp.generated.resources.general_error
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ErrorComposable(modifier: Modifier = Modifier) {
+    Box(
+        modifier =
+            modifier
+                .fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(text = stringResource(Res.string.general_error))
+    }
 }
 
 @Preview
